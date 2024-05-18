47c59b0ed6bc5a6af60c60a62305ace84a98700a
vijay7755 <vijaysekar2295@gmail.com>
2024-05-18 23:03:13 +0530

    "update index.ts and create new scripts in package.json"

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 import { schema } from "./schema";
 import { context } from "./context";
 
 console.log("schema")
 // console.log(schema)
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 export const server = new ApolloServer({
   schema,
-  context
+  context,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()], 
 });
 
-const port = 3007;
+const port = process.env.PORT || 3000;  
 
 server.listen({ port }).then(({ url }) => {
   console.log(`server ready at ${url}`);
